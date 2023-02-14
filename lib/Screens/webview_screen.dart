9a1e5638a5daf082fae51886308013ae2276507b
NaoufelMaazouzi <naoufel_du_28@live.fr>
2023-02-14 17:53:03 +0100

    add webview and refacto code

---
+++ lib/Screens/webview_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewScreen extends StatelessWidget {
+  const WebViewScreen({super.key});
+
+  static const routeName = '/webviewScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    // Extract the arguments from the current ModalRoute
+    // settings and cast them as ScreenArguments.
+    final args = ModalRoute.of(context)!.settings.arguments as ScreenArguments;
+
+    final webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(args.url));
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Profil Github'),
+        ),
+        body: WebViewWidget(controller: webViewController));
+  }
+}
+
+class ScreenArguments {
+  final String url;
+
+  ScreenArguments(this.url);
+}
